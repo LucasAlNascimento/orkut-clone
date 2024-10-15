19255aeae6b35f3004ad661204a070a7d07108cf
LucasAlNascimento <lucasalvesnascimento.lan@gmail.com>
2024-10-15 18:31:31 -0300

    feat: create interface loginform and toast notification

--- orkut-clone/src/app/pages/login/login.component.ts
+++ orkut-clone/src/app/pages/login/login.component.ts
@@ -5,6 +5,12 @@ import { PrimaryInputComponent } from '../../components/primary-input/primary-in
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEye } from '@fortawesome/free-regular-svg-icons';
 import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
+
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
 
 @Component({
   selector: 'app-login',
@@ -22,10 +28,10 @@ import { LoginService } from '../../services/login.service';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  loginForm!: FormGroup;
+  loginForm!: FormGroup<LoginForm>;
   faEye = faEye;
 
-  constructor(private loginService: LoginService){
+  constructor(private loginService: LoginService, private toastService: ToastrService){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
@@ -34,8 +40,8 @@ export class LoginComponent {
 
   submit(){
     this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: () => console.log("sucesso"),
-      error: () => console.log("erro")
+      next: () => this.toastService.success("Login efetuado!"),
+      error: () => this.toastService.error("Erro! Tente novamente mais tarde")
     })
   }
 }
