146732f60cbb6be6f706f3f73977567445407312
LucasAlNascimento <lucasalvesnascimento.lan@gmail.com>
2024-10-15 17:31:43 -0300

    feat: create submit functionality

--- orkut-clone/src/app/components/default-login-layout/default-login-layout.component.ts
+++ orkut-clone/src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './default-login-layout.component.html',
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
   @Input() title: string = "";
   @Input() btnText: string = "";
+  @Output("submit") onSubmit = new EventEmitter();
+
+  submit(){
+    this.onSubmit.emit();
+  }
 }
--- orkut-clone/src/app/pages/login/login.component.ts
+++ orkut-clone/src/app/pages/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -9,18 +12,30 @@ import { PrimaryInputComponent } from '../../components/primary-input/primary-in
   imports: [
     DefaultLoginLayoutComponent,
     ReactiveFormsModule,
-    PrimaryInputComponent
+    PrimaryInputComponent,
+    FontAwesomeModule
+  ],
+  providers: [
+    LoginService
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
+  faEye = faEye;
 
-  constructor(){
+  constructor(private loginService: LoginService){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  submit(){
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => console.log("sucesso"),
+      error: () => console.log("erro")
     })
   }
 }
