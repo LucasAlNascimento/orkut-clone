bc06eb41e361f80d579cf4377c7b811a40473534
LucasAlNascimento <lucasalvesnascimento.lan@gmail.com>
2024-10-17 19:49:32 -0300

    build: add firebase

--- orkut-clone/src/app/app.config.ts
+++ orkut-clone/src/app/app.config.ts
@@ -3,14 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC22APvXuAqhOyahQUrs6WUOhZTvTlrkv0",
+  authDomain: "clone-2f4b9.firebaseapp.com",
+  projectId: "clone-2f4b9",
+  storageBucket: "clone-2f4b9.appspot.com",
+  messagingSenderId: "632642563817",
+  appId: "1:632642563817:web:a8758743e8d7107e7a0a78"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
     provideToastr(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth())
   ]
 };
